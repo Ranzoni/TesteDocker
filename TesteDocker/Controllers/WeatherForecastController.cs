a4e2d02e38808f9a5407d706edc573d033d157df
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2022-04-13 10:36:00 -0300

    Alteração de teste

--- TesteDocker/Controllers/WeatherForecastController.cs
+++ TesteDocker/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TesteDocker.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Fefis", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Fefis Admin", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
