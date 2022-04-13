875b3cc3f3cb52fcbd638ef98e3634036a1053c9
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2022-04-13 10:36:54 -0300

    Teste commit

--- TesteDocker/Controllers/WeatherForecastController.cs
+++ TesteDocker/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TesteDocker.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Fefis Admin", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Fefis Admin 1", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
