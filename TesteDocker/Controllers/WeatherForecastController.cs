36da267f797ee3de05c005e4dbee6bdbb024c59c
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2022-04-13 10:48:48 -0300

    Teste

--- TesteDocker/Controllers/WeatherForecastController.cs
+++ TesteDocker/Controllers/WeatherForecastController.cs
@@ -6,10 +6,6 @@ namespace TesteDocker.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Fefis Admin 10", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -19,15 +15,9 @@ namespace TesteDocker.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Oi";
         }
     }
 }
