df6cfb6e2e7c32093fd6a953631c506522a3d992
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2022-04-29 11:04:32 -0300

    Teste 2

--- TesteDocker/Controllers/WeatherForecastController.cs
+++ TesteDocker/Controllers/WeatherForecastController.cs
@@ -17,7 +17,11 @@ namespace TesteDocker.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public string Get()
         {
-            return "Olá, mundo!!!!!";
+            var weatherForecast = new WeatherForecast()
+            {
+                Date = DateTime.Now
+            };
+            return weatherForecast.Date.ToString();
         }
     }
 }
